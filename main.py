059592a7d9e13596ca500458e0d30098bbff884c
DakyswR <116316120+PGN9@users.noreply.github.com>
2025-07-23 01:04:13 +0000

    added uvicorn in requirements, main fix

--- main.py
+++ main.py
@@ -11,22 +11,28 @@ start_time = time.time()
 app = FastAPI()
 MODEL_BACKEND_URL = "https://vader-backend-po5q.onrender.com/predict"  # the URL where vader-backend is deployed
 
+
 class TextListRequest(BaseModel):
     texts: List[str]
+
 
 @app.get("/")
 def root():
     return {"message": "proxy backend is running."}
 
+
 @app.post("/analyze")
-def analyze_sentiment(request: TextRequest):
+def analyze_sentiment(request: TextListRequest):
     try:
-        response = requests.post(MODEL_BACKEND_URL, json={"texts": request.texts})
+        response = requests.post(MODEL_BACKEND_URL,
+                                 json={"texts": request.texts})
         response.raise_for_status()
         return {"source": "proxy_backend", "model_response": response.json()}
     except requests.RequestException as e:
-        raise HTTPException(status_code=502, detail=f"Error calling vader_backend: {e}")
-    
+        raise HTTPException(status_code=502,
+                            detail=f"Error calling vader_backend: {e}")
+
+
 @app.middleware("http")
 async def count_requests(request: Request, call_next):
     global request_count, start_time
@@ -35,7 +41,9 @@ async def count_requests(request: Request, call_next):
 
     elapsed = time.time() - start_time
     if elapsed > 60:
-        print(f"[Request Stats] Last 60s - Requests: {request_count}, RPS: {request_count / elapsed:.2f}")
+        print(
+            f"[Request Stats] Last 60s - Requests: {request_count}, RPS: {request_count / elapsed:.2f}"
+        )
         request_count = 0
         start_time = time.time()
 
